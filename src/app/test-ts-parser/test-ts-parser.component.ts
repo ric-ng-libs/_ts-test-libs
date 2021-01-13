37568109471efdeda8ed0a5b24abe0d115f20ce4
Richard PYT <rick.info.rsoc@gmail.com>
2021-01-13 01:33:36 +0100

    tests "foetus parser" en cours

--- src/app/test-ts-parser/test-ts-parser.component.ts
+++ src/app/test-ts-parser/test-ts-parser.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 
-// import { 
-//   GenericList
-// } from '@ric-ng/ts-general'; // cf. tsconfig.json (baseUrl+paths), la rech. de ce chemin
-//                                           // NON relatif, mènera à ./dist/ts-general/package.json -
-//                                           //  (champ typings...)
-//                                           // MAIS ATTENTION, cette syntaxe d'import n'est surtout pas celle à
-//                                           // utiliser depuis le code des sources de ma lib ! (dans /projects/)
-//                                           // Car en prod. (répertoire dist/ publié et "NPM-installé"),
-//                                           // le tsconfig.json n'existera pas !!
-//                                           // Donc la syntaxe d'import à utiliser dans les sources de ma lib,
-//                                           // sera :
-//                                           //  - soit, avec chemin relatif pour faire réf. à des sources de
-//                                           //    la lib. elle-même.
-//                                           //  - soit du style : import ... from '@aaa/bbb'
-//                                           //    avec '@aaa', organisation/namespace NPM où a été publié
-//                                           //    le package 'bbb', dont donc dépend notre lib.
-//                                           //    En pareil cas, ce package devra ici être installé via NPM i,
-//                                           //    (donc dans /node_modules/@aaa/bbb/), afin de ne pas avoir
-//                                           //    d'erreur de compil./build ici.
+import { 
+  IPattern, 
+  StringPattern,
+  RegExpStringPattern,
+  IPatternsList,
+  OrderedFullMatchPatternsList,
+  OrderedOneMatchPatternsList, 
+
+  IStringToParse,
+  StringToParse,
+  IStringToParseMatchingsList,
+  StringToParseMatchingsListOrNull,
+  StringToParseMatchingsList,
+} from '@ric-ng/ts-parser';
+
+
+
 
 @Component({
   selector: 'app-test-ts-parser',
@@ -32,8 +30,128 @@ export class TestTsParserComponent implements OnInit {
   libraryDescription: string = "Parser";  
 
 
+
+  stringToParseMatchingsList: IStringToParseMatchingsList;
+
+
   constructor() {
+    this.test1( this.getStringToParse() );
   }
+  
+  private test1(stringToParse: IStringToParse): void {
+    const patternsList: OrderedOneMatchPatternsList = new OrderedOneMatchPatternsList();
+    
+    const regExpStringPattern1: IPattern = new RegExpStringPattern("[a-z0-9]+", false);
+
+    const stringPattern1: IPattern = new StringPattern("DEB");
+    const stringPattern2: IPattern = new StringPattern("\n");
+    const stringPattern3: IPattern = new StringPattern("MID", false);
+    const stringPattern4: IPattern = new StringPattern("FIN");
+
+    patternsList.addPatterns([
+      // stringPattern1,
+      regExpStringPattern1,
+      stringPattern2,
+      stringPattern3,
+      stringPattern2,
+      stringPattern4
+    ]);
+
+    this.stringToParseMatchingsList = patternsList.getStringToParseMatchings( stringToParse );
+
+    // stringToParse.incrementPointerPosition(this.stringToParseMatchingsList.getTotalLength());
+
+    console.log(stringToParse.getPointerPosition());
+  }
+
+  private getStringToParse(): IStringToParse {
+    const result: IStringToParse = new StringToParse( this.getStringToParseAsString() );
+    return(result);
+  }
+  
+  private getStringToParseAsString(): string {
+    const result: string = [
+      // "DEB",
+      "ù ù ù",
+      "KbJardin",
+      "aZ99Hu",
+      "mid",
+      "FIN",
+      "autre"
+    ].join("\n");
+    return(result);
+  }
+
+
+
+    //@return {Array<IStringToParseMatching> | null} null if matching fails.
+    private getPatternStringToParseMatchings(pattern: IPattern, stringToParse: IStringToParse)
+        : StringToParseMatchingsListOrNull {
+
+        let result: StringToParseMatchingsListOrNull = null;
+        if (!stringToParse.isPointerAtTheEnd()) {
+            let isBadMatchingOccurencesNumber: boolean = false;
+    
+            let stringToParseMatchings: StringToParseMatchingsListOrNull;
+            let isStringToParseMatchings: boolean;
+
+            result = new StringToParseMatchingsList();
+            stringToParse.savePointerPosition();
+            do {
+                stringToParseMatchings = pattern.getStringToParseMatchings(
+                    stringToParse
+                );
+                
+                isStringToParseMatchings = (stringToParseMatchings !== null);
+                if (isStringToParseMatchings)  {
+
+                    result.addElementsFromList( stringToParseMatchings );
+                    
+                    isBadMatchingOccurencesNumber = this.hasFoundTooManyMatchingOccurences(
+                        pattern, 
+                        result.getElementsNumber()
+                    );
+    
+                    stringToParse.incrementPointerPosition(stringToParseMatchings.getTotalLength());
+                }            
+    
+            } while(isStringToParseMatchings && !isBadMatchingOccurencesNumber && !stringToParse.isPointerAtTheEnd());
+    
+    
+            if (!isBadMatchingOccurencesNumber) {
+                isBadMatchingOccurencesNumber = this.hasFoundNotEnoughMatchingOccurences(
+                    pattern, 
+                    result.getElementsNumber()
+                );
+            }
+    
+            if (isBadMatchingOccurencesNumber) {
+                result = null;
+                stringToParse.restoreLastSavedPointerPosition();
+
+            } else {
+                stringToParse.cancelLastSavedPointerPosition();
+
+            }
+        }        
+
+        return(result);
+    }
+    
+    private hasFoundTooManyMatchingOccurences(pattern: IPattern, matchingOccurencesNumber: number): boolean {
+        let result: boolean = false;
+
+        if (pattern.isDefinedMaxOccurencesNumber()) {
+            result = (matchingOccurencesNumber > pattern.getMaxOccurencesNumber());
+        }
+
+        return(result);
+    }
+
+    private hasFoundNotEnoughMatchingOccurences(pattern: IPattern, matchingOccurencesNumber: number): boolean {
+        const result: boolean =  (matchingOccurencesNumber < pattern.getMinOccurencesNumber());
+        return(result);
+    }  
   
 
   ngOnInit() {
