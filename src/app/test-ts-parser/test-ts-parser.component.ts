8cd07a56cbff389a13811158aa296b3a7e80fc15
Richard PYT <rick.info.rsoc@gmail.com>
2021-01-15 18:10:08 +0100

    Quelques tests supplémetaires (et ajustements de code dans ts-parser) + début tests de blocs...

--- src/app/test-ts-parser/test-ts-parser.component.ts
+++ src/app/test-ts-parser/test-ts-parser.component.ts
@@ -49,21 +49,47 @@ export class TestTsParserComponent {
 
     const stringPattern0: IStringPattern = patternsFactory.getStringPattern(" ", 0, null);
     const stringPattern1: IStringPattern = patternsFactory.getStringPattern("let ", 1, null, true);
-    const stringPattern2: IStringPattern = patternsFactory.getStringPattern("=", 1, null, true);
-    const stringPattern3: IStringPattern = patternsFactory.getStringPattern(" ", 1, null, false);
-    const stringPattern4: IStringPattern = patternsFactory.getStringPattern("FIN", 1, null, true);
     const regExpStringPattern1: IRegExpStringPattern = patternsFactory.getRegExpStringPattern("[a-z]{1}[a-z0-9]*[ ]*", 1, 1, false);
-    const regExpStringPattern2: IRegExpStringPattern = patternsFactory.getRegExpStringPattern("[ ]*", 1, 1, false);
-    const regExpStringPattern3: IRegExpStringPattern = patternsFactory.getRegExpStringPattern("[0-9]+[ ]*;", 1, 1, false);
+    const stringPattern2: IStringPattern = patternsFactory.getStringPattern("=", 1, null, true);
+    // const stringPattern3: IStringPattern = patternsFactory.getStringPattern(" ", 0, null, false);
+    // const stringPattern4: IStringPattern = patternsFactory.getStringPattern("FIN", 1, null, true);
+    const regExpStringPattern2: IRegExpStringPattern = patternsFactory.getRegExpStringPattern("[ ]*", 0, 1, false);
+    const regExpStringPattern3: IRegExpStringPattern = patternsFactory.getRegExpStringPattern("[0-9]+[ ]*;", 0, 1, false);
 
+    const spaces0Null: IStringPattern = patternsFactory.getStringPattern(" ", 0, null);
+    const spaces1Null: IStringPattern = patternsFactory.getStringPattern(" ", 1, null);
+    const exportKeyWord: IStringPattern = patternsFactory.getStringPattern("export", 1, 1);
+    const abstractKeyWord: IStringPattern = patternsFactory.getStringPattern("abstract", 1, 1);
+    const classKeyWord: IStringPattern = patternsFactory.getStringPattern("class", 1, 1);
+    const identifier: IRegExpStringPattern = patternsFactory.getRegExpStringPattern("[a-z]{1}[a-z0-9]*", 1, 1, false);
+    const blockStartToken: IStringPattern = patternsFactory.getStringPattern("{", 1, 1);
+
+    const blockEndToken: IStringPattern = patternsFactory.getStringPattern("}", 1, 1);
+    const regExpAnyThingBetween: IRegExpStringPattern = patternsFactory.getRegExpStringPattern("\{(.*)\}", 0, 1, false);
 
     const patternsList: IPatternsList = patternsFactory.getOrderedFullMatchPatternsList([
-      stringPattern0,
-      stringPattern1,
-      regExpStringPattern1,
-      stringPattern2,
-      regExpStringPattern2,
-      regExpStringPattern3
+      // spaces0Null,
+      
+      // patternsFactory.getOrderedFullMatchPatternsList([exportKeyWord, spaces1Null], 0, 1),
+
+      // patternsFactory.getOrderedFullMatchPatternsList([abstractKeyWord, spaces1Null], 0, 1),
+
+      // patternsFactory.getOrderedFullMatchPatternsList([classKeyWord, spaces1Null], 1, 1),
+
+      // identifier,
+
+      // patternsFactory.getOrderedFullMatchPatternsList([spaces0Null, blockStartToken], 1, 1),
+      
+      // blockEndToken
+      blockStartToken, regExpAnyThingBetween, blockEndToken
+
+      // stringPattern0,
+      // stringPattern1,
+      // regExpStringPattern1,
+      // stringPattern2,
+      // regExpStringPattern2,
+      // stringPattern2,
+      // regExpStringPattern3
       // stringPattern3,
       // stringPattern2,
       // stringPattern4
@@ -81,7 +107,12 @@ export class TestTsParserComponent {
 
   private getStringToParseAsString(): string {
     const result: string = [
-      "let toto = 5;"
+
+      // "  export     abstract   class NomClasse{}"
+      // "  class NomClasse  {}"
+      "{nimporte}"
+      //"let toto =5;"
+      //"= 55   ;"
       // "ù ù ù",
       // "KbJardin",
       // "aZ99Hu",
